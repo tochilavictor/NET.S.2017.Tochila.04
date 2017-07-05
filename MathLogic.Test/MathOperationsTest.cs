b80394c93d5d04170c08a59831625095fe563007
tochilavictor <tochilavictor@gmail.com>
2017-07-05 22:13:21 +0300

    Add project files.

---
+++ MathLogic.Test/MathOperationsTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MathLogic;
+
+namespace MathLogic.Test
+{
+    [TestFixture]
+    public class MathOperationsTest
+    {
+        [TestCase(8, 3, 0.001, 2)]
+        [TestCase(25, 2, 0.00001, 5)]
+        [TestCase(27, 3, 0 , 3)]
+        [TestCase(1024, 10, 0.00001, 2)]
+        [TestCase(10, 2, 0.001, 3.1622)]
+        public void SqrtNewton_GoodRangesAndResults_PositiveTest(double number, int power, double accuracy,double expectedResult)
+        {
+            double actual = MathOperations.SqrtNewton(number, power, accuracy);
+
+            Assert.True(Math.Abs(actual - expectedResult) <= accuracy);
+        }
+
+        [TestCase(4, ExpectedResult = 2)]
+        [TestCase(25, ExpectedResult = 5)]
+        [TestCase(13689, ExpectedResult = 117)]
+        [TestCase(121, ExpectedResult = 11)]
+        public double SqrtNewton_OneArgument_PositiveTest(double number)
+        {
+            return MathOperations.SqrtNewton(number);
+        }
+
+        [TestCase(8, 2, 0.00001, 3)]
+        [TestCase(25, 2, 0.001, 3)]
+        [TestCase(27, 3, 0.00001, 3.5)]
+        [TestCase(4, 2, 0.00001, 2.02)]
+        public void SqrtNewton_GoodRangesAndBadResults_NegativeTest(double number, int power, double accuracy, double expectedResult)
+        {
+            double actual = MathOperations.SqrtNewton(number, power, accuracy);
+
+            Assert.False(Math.Abs(actual - expectedResult) <= accuracy);
+        }
+
+        [TestCase(-8, 2, 0.00001, 3)]
+        [TestCase(25, -2, 0.001, 3)]
+        [TestCase(27, 3,- 0.00001, 3.5)]
+        [TestCase(-4,-2, 0.00001, 2.02)]
+        public void SqrtNewton_NegativeNumberOrPowerOrAccuracy_ExpectedIndexOutOfRangeException(double number, int power, double accuracy, double expectedResult)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => MathOperations.SqrtNewton(number, power, accuracy));
+        }
+    }
+}
---
+++ MathLogic/MathOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLogic
+{
+    public static class MathOperations
+    {
+        /// <summary>
+        /// implements Newton algorythm for getting any natural power root from number
+        /// </summary>
+        /// <param name="number">number</param>
+        /// <param name="power">natural power of root</param>
+        /// <param name="accuracy">minimum accuracy</param>
+        /// <returns>root value (value^power=number)</returns>
+        /// <exception cref="ArgumentOutOfRangeException">using negative number,power or accuracy</exception>
+        public static double SqrtNewton(double number, int power = 2 , double accuracy = 0)
+        {
+            if (number < 0) throw new ArgumentOutOfRangeException($"{nameof(number)} value must be > 0");
+            if (power < 0) throw new ArgumentOutOfRangeException($"{nameof(power)} value must be natural");
+            if (accuracy < 0) throw new ArgumentOutOfRangeException($"{nameof(accuracy)} value must be > 0");
+
+            double x = number, delta = x;
+
+            while (delta > accuracy)
+            {
+                var prevx = x;
+                x = (1.0 / (double)power) * ((power - 1) * x + number / Math.Pow(x, power - 1));
+                delta = Math.Abs(prevx - x);
+            }
+
+            return x;
+        }
+    }
+}
